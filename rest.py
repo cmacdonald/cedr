2a3c3ac3922ed6107cfe0fc0095e1592c816c263
Craig Macdonald <craig.macdonald_AT_glasgow.ac.uk>
2019-07-27 23:44:24 +0100

    initial commit of a rest API for scoring

---
+++ rest.py
@@ -0,0 +1,80 @@
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
+import pandas as pd
+import sys
+import pickle
+
+import argparse
+import train
+
+#This script make an HTTP REST endpoint that exposes a POST method that can score documents:
+#the post expected a JSON object in the following format:
+#{ qid: 'query1', query:'information retrieval', docnos : ['d1', 'd2'], 'docs':['keith proposed information retrieval', 'not matching doc']}
+
+
+#e.g. curl -d '{ "qid": "query1", "query":"information retrieval", "docnos" : ["d1", "d2"], "docs":["keith proposed information retrieval", "not matching doc"]}'  -H "Content-Type: application/json" -X POST http://localhost:3422/
+
+class CEDR(Resource):
+    
+
+    def __init__(self, CEDRmodel):
+        self.model = CEDRmodel
+        self.requestCount = 0
+    
+    def get(self):
+        return "Invalid request - only POST supported, you called get. " + str(self.requestCount) + " requests served thus far", 400
+
+    def post(self):
+        self.requestCount+=1
+        
+        parser = reqparse.RequestParser()
+        parser.add_argument("qid", location='json')
+        parser.add_argument("query", location='json')
+        parser.add_argument("docnos", location='json', type=list)
+        parser.add_argument("docs", location='json', type=list)
+        args = parser.parse_args()
+
+        print("Processing request %d, query %s, getting scores for %d documents" % (self.requestCount, args['query'], len(args['docs'])) )
+        
+        # Data now in args['name']. e.g., args['DOC'].
+        df = pd.DataFrame({
+            'text_left': [args['query']]*len(args['docs']),
+            'text_right': args['docs'],
+            'id_left': [args['qid']]*len(args['docs']),
+            'id_right': args['docnos']
+        })
+        scores = train.score_docsMZ(self.model, df) 
+        print(scores.flatten().tolist())        
+        response = jsonify(scores.flatten().tolist())
+        response.status_code = 200
+        return response
+
+    def put(self):
+        return "Invalid request - only POST supported, you called put", 400
+
+    def delete(self):
+        return "Invalid request - only POST supported, you called delete", 400
+
+
+
+
+def main_cli():
+    parser = argparse.ArgumentParser('CEDR model re-ranking REST API')
+    parser.add_argument('--model', choices=train.MODEL_MAP.keys(), default='vanilla_bert')
+    parser.add_argument('--model_weights', type=argparse.FileType('rb'))
+    parser.add_argument('--port', default=5678)
+    
+    args = parser.parse_args()
+    model = train.MODEL_MAP[args.model]()#.cuda()
+    if args.model_weights is not None:
+        model.load(args.model_weights.name)
+    #train.run_model(model, dataset, run, args.out_path.name, desc='rerank')
+
+    app = Flask(__name__)
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+    api = Api(app)
+    api.add_resource(CEDR, "/", resource_class_kwargs={'CEDRmodel':model})
+    app.run(host='0.0.0.0',port=args.port,debug=True)
+
+if __name__ == '__main__':
+    main_cli()
