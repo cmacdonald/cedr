f6c0f1aee481ac3988ec5d2a9bd63850d70aebfc
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2019-08-05 14:00:36 +0100

    flushing helps when using in YARN

--- trainMZdataframe.py
+++ trainMZdataframe.py
@@ -3,13 +3,14 @@ import argparse
 import data
 import os
 import pandas as pd
+import signal
+import traceback
 
 def main(trainTable, validTable, qrelsFile, modelName, bertWeights, saveDirectory):
     docs={}
     queries={}
+
     for index, row in trainTable.iterrows():
-        #print(type(row))
-        #print(row)
         queries[row['id_left']] = row['text_left']
         docs[row['id_right']] = row['text_right']
     for index, row in validTable.iterrows():
@@ -35,7 +36,7 @@ def main(trainTable, validTable, qrelsFile, modelName, bertWeights, saveDirector
 
     if bertWeights is not None:
         model.load(bertWeights)
-
+    #import pdb; pdb.set_trace()
     train.main(model, dataset, train_pairs, qrels, valid_run, qrelsFile, saveDirectory)
 
 def main_cli():
@@ -48,15 +49,16 @@ def main_cli():
     parser.add_argument('--model_out_dir')
     args = parser.parse_args()
 
-    print("Reading training file %s" % args.trainTSV[0])
+    print("Reading training file %s" % args.trainTSV[0], flush=True)
     trainTable = pd.read_csv(args.trainTSV[0], sep='\t', header=0)
     print(trainTable.columns.values)
-    print("Reading validation file %s" % args.validTSV[0])
+    print("Reading validation file %s" % args.validTSV[0], flush=True)
     validTable = pd.read_csv(args.validTSV[0], sep='\t', header=0)
-    
     os.makedirs(args.model_out_dir, exist_ok=True)
-
+    signal.signal(signal.SIGUSR1, lambda sig, stack: traceback.print_stack(stack))
     main(trainTable, validTable, args.qrels.name, args.model, args.initial_bert_weights, args.model_out_dir)
+
+
 
 if __name__ == '__main__':
     main_cli()
